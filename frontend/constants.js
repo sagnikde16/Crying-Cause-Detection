2617273c3012a6144ce151c0f8e159d29a6f4843
sagnikde16 <sagnikde191@gmail.com>
2026-03-04 02:33:17 +0530

    changes

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,11 +1,12 @@
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
-const DEV_LOCAL_IP = '10.145.31.131';
-// const DEV_LOCAL_IP = 'localhost';
+// const DEV_LOCAL_IP = 'https://crying-cause-backend.onrender.com/';
+// // const DEV_LOCAL_IP = 'localhost';
 
-export const API_URL = Platform.select({
-  ios: `http://localhost:5000`,
-  android: `http://${DEV_LOCAL_IP}:5000`,
-  web: `http://${DEV_LOCAL_IP}:5000`,
-  default: `http://${DEV_LOCAL_IP}:5000`
-});
+// export const API_URL = Platform.select({
+//   ios: `http://localhost:5000`,
+//   android: `http://${DEV_LOCAL_IP}:5000`,
+//   web: `http://${DEV_LOCAL_IP}:5000`,
+//   default: `http://${DEV_LOCAL_IP}:5000`
+// });
+export const API_URL = 'https://crying-cause-backend.onrender.com';
